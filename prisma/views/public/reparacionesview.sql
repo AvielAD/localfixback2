b84eb2c58fd75d849044cf59820327d6ccdb3c9b
Aviel Aldama <aviel.aldama@gmail.com>
2024-01-29 23:11:42 -0600

    reparacion view

---
+++ prisma/views/public/reparacionesview.sql
@@ -0,0 +1,34 @@
+SELECT
+  r.id,
+  r.uuidsearch AS uuid,
+  c.nombre,
+  c.apellido,
+  c.telefono,
+  r.fecharecepcion AS recepcion,
+  r.fechaentrega AS entrega,
+  e.modelopopular AS modelo,
+  b.nombre AS marca,
+  d.descripcionfalla AS falla,
+  d.sugerenciareparacion AS diagnostico,
+  d.costopresupuesto AS presupuesto,
+  r.costototal AS total
+FROM
+  (
+    (
+      (
+        (
+          (
+            (
+              reparacion_diagnostico rd
+              JOIN reparacion r ON ((r.id = rd.idreparacion))
+            )
+            JOIN diagnostico d ON ((d.id = rd.iddiagnostico))
+          )
+          JOIN equipos e ON ((e.id = d.idequipo))
+        )
+        JOIN brandequipo b ON ((b.id = e.idbrandequipo))
+      )
+      JOIN categoria_equipo ce ON ((ce.id = e.idcategoriaequipo))
+    )
+    JOIN cliente c ON ((c.id = r.idcliente))
+  );
